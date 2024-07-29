f12675f0cb69220864bd7f42bffbd37b89516183
Shashank Dubey <96064747+CorruptEntity0982@users.noreply.github.com>
2024-07-29 13:38:26 +0530

    Untracked Files

--- test.py
+++
@@ -1,4 +0,0 @@
-import requests
-COINGECKO_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=USD&order=market_cap_desc"
-response = requests.get(COINGECKO_URL)
-print(response.status_code, response.json())
